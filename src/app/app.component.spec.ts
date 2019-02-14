846066490fab3200670170f95c032c1c26b3234b
IsaiasMorochi <isaiasmorochi@hotmail.com>
2019-02-14 10:44:21 -0400

    navbar menu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        LandingPageComponent
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('Debe crear nuestra Aplicación', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cinemax'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('cinemax');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to cinemax!');
-  });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,23 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NavbarSocialComponent } from './landing-page/navbar-social/navbar-social.component';
 
 
+import { LandingPageService } from './services/landing-page.service';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarMenuComponent } from './landing-page/navbar-menu/navbar-menu.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     NavbarSocialComponent,
+    NavbarMenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [LandingPageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
